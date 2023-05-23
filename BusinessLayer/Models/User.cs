531bfb0263815a3f3488215e348f20b494be17e9
Sandro Rusitashvili <rusitashvilisandro@gmail.com>
2023-05-23 21:17:19 +0400

    introduction of basic models
    add some of the basic models

---
+++ BusinessLayer/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+        public string UserName { get; set; }
+        public string PasswordSalt { get; set; }
+        public string HashedPassword { get; set; }
+        public string Email { get; set; }
+
+    }
+}

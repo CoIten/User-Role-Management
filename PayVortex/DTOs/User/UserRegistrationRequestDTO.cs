4e30ffe2c9a4530a266c71086c5a554f436f3818
Sandro Rusitashvili <rusitashvilisandro@gmail.com>
2023-05-27 00:44:34 +0400

    add user dto

---
+++ PayVortex/DTOs/User/UserRegistrationRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Models.Users;
+
+namespace Web.DTOs.User
+{
+    public class UserRegistrationRequestDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}

968a73fc3af86905651ab8b67f550cd6199cb7e7
Sandro Rusitashvili <rusitashvilisandro@gmail.com>
2023-05-30 13:31:44 +0400

    Create RoleController.cs

---
+++ Web/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+        private readonly IMapper _mapper;
+
+        public RoleController(IRoleService roleService, IMapper mapper)
+        {
+            _roleService = roleService;
+            _mapper = mapper;
+        }
+
+        //[HttpGet("roleId")]
+        //public async Task<ActionResult<RoleDTO)>> GetRoleByIdAsync(int roleId)
+        //{
+        //    var role = await _roleService.GetRoleByIdAsync(roleId);
+        //    if (role == null)
+        //        return NotFound();
+        //    var role
+        //}
+
+        //[HttpGet()]
+    }
+}
